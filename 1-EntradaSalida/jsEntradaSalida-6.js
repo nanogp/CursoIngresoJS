9a4c564d550b3db088df278a5c92c28bf9842d28
nanogp <nanogp@users.noreply.github.com>
2016-11-14 19:48:08 -0300

    Primer clase
    Primer clase

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,9 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	// numeroUno numeroDos
+	var uno 		= document.getElementById('numeroUno').value;
+	var dos 		= document.getElementById('numeroDos').value;
+	var resultado 	= parseInt(uno) + parseInt(dos);
+	alert("la suma es " + resultado);
 }
-
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,34 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	// numeroUno numeroDos
+	var uno 		= document.getElementById('numeroUno').value;
+	var dos 		= document.getElementById('numeroDos').value;
+	var resultado 	= parseInt(uno) + parseInt(dos);
+	alert("la Suma es " + resultado);
 }
 
 function restar()
 {
-	
+	var uno 		= document.getElementById('numeroUno').value;
+	var dos 		= document.getElementById('numeroDos').value;
+	var resultado 	= parseInt(uno) - parseInt(dos);
+	alert("la Resta es " + resultado);
 }
 
 function multiplicar()
 { 
-	
+	var uno 		= document.getElementById('numeroUno').value;
+	var dos 		= document.getElementById('numeroDos').value;
+	var resultado 	= parseInt(uno) * parseInt(dos);
+	alert("el Producto es " + resultado);
 }
 
 function dividir()
 {
-	
+	var uno = document.getElementById('numeroUno').value;
+	var dos = document.getElementById('numeroDos').value;
+	var resultado = parseInt(uno) / parseInt(dos);
+	alert("el Cociente es " + resultado);
 }
 
