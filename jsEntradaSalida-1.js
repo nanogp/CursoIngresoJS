95d5e9ccf69b753dc1ca9ea3e6fe614293131045
nanogp <nanogp@users.noreply.github.com>
2016-11-14 19:00:39 -0300

    Delete jsEntradaSalida-1.js

--- jsEntradaSalida-1.js
+++
@@ -1,7 +0,0 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
-function Mostrar()
-{
-	//window.alert sino, ventana modal
-	//window.confirm
-	alert("Esto funciona de maravilla");
-}
